15ca1caebcc094be2ef98744334e8c8a56255aa9
Ilya Kiyanov <ikiyanov@protonmail.com>
2018-09-07 03:49:29 +0300

    DRAFT. Adds first script and styles for closing modal-map.

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+var mapLink = document.querySelector(".btn-about-us");
+
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = document.querySelector(".close-map");
+
+mapLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove("modal-show");
+});
+
+/*window.addEventListener("keydown", function (evt) {
+  evt.preventDefault();
+  if (evt.keyCode === 27) {
+    if (mapPopup.classList.contains("modal-show")) {
+      mapPopup.classList.remove("modal-show");
+    }
+  }
+});*/
